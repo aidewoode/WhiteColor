5e891f2255206169470a612098c9b2471f7e1770
aidewoode <aidewoode@gmail.com>
2015-09-21 21:24:54 +0800

    Initial Commit

---
+++ WhiteColor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WhiteColor
+//
+//  Created by aidewoode on 15/9/21.
+//  Copyright © 2015年 aidewoode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WhiteColor/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WhiteColor
+//
+//  Created by aidewoode on 15/9/21.
+//  Copyright © 2015年 aidewoode. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
